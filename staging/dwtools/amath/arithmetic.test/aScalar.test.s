2a7f9a5b697e2d5ca575df0d7b4f423df22a39c0
PabloGarcimartin <39849351+PabloGarcimartin@users.noreply.github.com>
2018-06-03 08:12:05 +0200

    Round/Floor and Ceil to the power of 2 unit test routines written
    Test routines for the functions "round/floor/ceil to the power of 2" have been added.

--- staging/dwtools/amath/arithmetic.test/aScalar.test.s
+++ staging/dwtools/amath/arithmetic.test/aScalar.test.s
@@ -68,7 +68,7 @@ function fract( test )
 
   test.description = 'zero';
   test.equivalent( _.fract( 0 ) , 0.0 );
-  
+
     test.description = 'two decimals';
   test.equivalent( _.fract( 2.15 ) , 0.15 );
 
@@ -108,7 +108,7 @@ function factorial( test )
 
   test.description = '1!';
   test.equivalent( _.factorial( 1 ) , 1 );
-  
+
   test.description = '2!';
   test.equivalent( _.factorial( 2 ) , 2 );
 
@@ -117,14 +117,13 @@ function factorial( test )
 
   test.description = '4!';
   test.equivalent( _.factorial( 4 ) , 24 );
-  
+
    test.description = '5!';
   test.equivalent( _.factorial( 5 ) , 120 );
-  
+
   test.description = '10!';
   test.equivalent( _.factorial( 10 ) , 3628800 );
 
-  
   /* */
 
   if( !Config.debug )
@@ -153,7 +152,7 @@ function factorial( test )
   {
     _.factorial( 1,3 );
   });
-  
+
   test.description = 'negative argument';
   test.shouldThrowError( function()
   {
@@ -165,13 +164,164 @@ function factorial( test )
   {
     _.factorial( 2.5 );
   });
-  
+
    test.description = 'Zero';
   test.shouldThrowError( function()
   {
     _.factorial( 0 );
   });
-  
+
+}
+
+
+function roundToPowerOfTwo( test )
+{
+
+  test.description = 'roundToPowerOfTwo: 1';
+  test.equivalent( _.roundToPowerOfTwo( 1 ) , 1 );
+
+  test.description = 'roundToPowerOfTwo: 127';
+  test.equivalent( _.roundToPowerOfTwo( 127 ) , 128 );
+
+  test.description = 'roundToPowerOfTwo: 127.5';
+  test.equivalent( _.roundToPowerOfTwo( 127.5 ) , 128 );
+
+  test.description = 'roundToPowerOfTwo: 11';
+  test.equivalent( _.roundToPowerOfTwo( 11 ) , 8 );
+
+  /* */
+
+  if( !Config.debug )
+  return;
+
+  test.description = 'no arguments';
+  test.shouldThrowError( function()
+  {
+    _.roundToPowerOfTwo();
+  });
+
+    test.description = 'wrong type of argument';
+    test.shouldThrowError( function()
+    {
+      _.roundToPowerOfTwo( 'x' );
+    });
+
+    test.description = 'wrong type of argument';
+    test.shouldThrowError( function()
+    {
+      _.roundToPowerOfTwo( [] );
+    });
+
+    test.description = 'too many arguments';
+    test.shouldThrowError( function()
+    {
+      _.roundToPowerOfTwo( 1,3 );
+    });
+
+    test.description = 'negative argument';
+    test.shouldThrowError( function()
+    {
+      _.roundToPowerOfTwo( -4 );
+    });
+
+}
+
+function ceilToPowerOfTwo( test )
+{
+
+  test.description = 'ceilToPowerOfTwo: 127';
+  test.equivalent( _.ceilToPowerOfTwo( 127 ) , 128 );
+
+  test.description = 'ceilToPowerOfTwo: 127.5';
+  test.equivalent( _.ceilToPowerOfTwo( 127.5 ) , 128 );
+
+  test.description = 'ceilToPowerOfTwo: 15';
+  test.equivalent( _.ceilToPowerOfTwo( 15 ) , 16 );
+
+  /* */
+
+  if( !Config.debug )
+  return;
+
+  test.description = 'no arguments';
+  test.shouldThrowError( function()
+  {
+    _.ceilToPowerOfTwo();
+  });
+
+    test.description = 'wrong type of argument';
+    test.shouldThrowError( function()
+    {
+      _.ceilToPowerOfTwo( 'x' );
+    });
+
+    test.description = 'wrong type of argument';
+    test.shouldThrowError( function()
+    {
+      _.ceilToPowerOfTwo( [] );
+    });
+
+    test.description = 'too many arguments';
+    test.shouldThrowError( function()
+    {
+      _.ceilToPowerOfTwo( 1,3 );
+    });
+
+    test.description = 'negative argument';
+    test.shouldThrowError( function()
+    {
+      _.ceilToPowerOfTwo( -4 );
+    });
+
+}
+
+function floorToPowerOfTwo( test )
+{
+
+  test.description = 'floorToPowerOfTwo: 19';
+  test.equivalent( _.floorToPowerOfTwo( 19 ) , 16 );
+
+  test.description = 'floorToPowerOfTwo: 31.9';
+  test.equivalent( _.floorToPowerOfTwo( 31.9 ) , 16 );
+
+  test.description = 'floorToPowerOfTwo: 0';
+  test.equivalent( _.floorToPowerOfTwo( 0 ) , 0 );
+
+  /* */
+
+  if( !Config.debug )
+  return;
+
+  test.description = 'no arguments';
+  test.shouldThrowError( function()
+  {
+    _.floorToPowerOfTwo();
+  });
+
+    test.description = 'wrong type of argument';
+    test.shouldThrowError( function()
+    {
+      _.floorToPowerOfTwo( 'x' );
+    });
+
+    test.description = 'wrong type of argument';
+    test.shouldThrowError( function()
+    {
+      _.floorToPowerOfTwo( [] );
+    });
+
+    test.description = 'too many arguments';
+    test.shouldThrowError( function()
+    {
+      _.floorToPowerOfTwo( 1,3 );
+    });
+
+    test.description = 'negative argument';
+    test.shouldThrowError( function()
+    {
+      _.floorToPowerOfTwo( -4 );
+    });
+
 }
 
 
@@ -192,7 +342,9 @@ var Self =
 
     fract : fract,
     factorial : factorial,
-
+    roundToPowerOfTwo : roundToPowerOfTwo,
+    ceilToPowerOfTwo : ceilToPowerOfTwo,
+    floorToPowerOfTwo : floorToPowerOfTwo,
   },
 
 };
