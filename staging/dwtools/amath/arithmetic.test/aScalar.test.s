5cddc2cca7520a0f8ed0f183a14c6cdf9d27dae0
Wandalen <wandalen.me@gmail.com>
2018-11-03 12:44:57 +0200

    auto

--- staging/dwtools/amath/arithmetic.test/aScalar.test.s
+++ staging/dwtools/amath/arithmetic.test/aScalar.test.s
@@ -33,7 +33,7 @@ if( typeof module !== 'undefined' )
 //
 
 var _ = _global_.wTools.withArray.Float32;
-var Parent = _.Tester;
+var Parent = /*_.*/wTester;
 
 // --
 // test
@@ -387,6 +387,6 @@ var Self =
 
 Self = wTestSuite( Self );
 if( typeof module !== 'undefined' && !module.parent )
-_.Tester.test( Self.name );
+/*_.*/wTester.test( Self.name );
 
 } )( );
